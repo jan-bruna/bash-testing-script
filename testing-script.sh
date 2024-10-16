7dd9765db0d94a089c31952b728d6aa77af3a70e
jan-bruna <jan.bruna03@gmail.com>
2024-10-16 21:16:41 +0200

    Add better customisation

--- testing-script.sh
+++ testing-script.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-PROGRAM=./a.out
+### PATHS AND NAMES you can change the paths and names of your files
+CODE=progtest.c
 SAMPLES_PATH=./sample/CZE
 SAMPLES_RANGE=({0000..0100})
+SAMPLE_SUFFIX_IN=_in.txt
+SAMPLE_SUFFIX_OUT=_out.txt
+PROGRAM=./a.out
+OUTPUT_FILE=test_output.txt
 
-g++ progtest.c
+### COMPILATION you can enable compilation so you don't test the old program
+g++ -Wall -pedantic -Wextra $CODE
 
+### SCRIPT you better not change anything ;)
 for X in ${SAMPLES_RANGE[@]} ; do
-    if [ ! -e ${SAMPLES_PATH}/${X}_in.txt ] ; then
+    if [ ! -e ${SAMPLES_PATH}/${X}$SAMPLE_SUFFIX_IN ] ; then
         echo "END"
 	exit 0
     fi
-    echo "Testing: ${X}_in.txt"
-    $PROGRAM < ${SAMPLES_PATH}/${X}_in.txt > ./test_output.txt
-    if ! diff -q ${SAMPLES_PATH}/${X}_out.txt ./test_output.txt ; then
-	echo "Fail: ${X}_in.txt"
+    echo "Testing: ${X}$SAMPLE_SUFFIX_OUT"
+    $PROGRAM < ${SAMPLES_PATH}/${X}$SAMPLE_SUFFIX_IN > ./$OUTPUT_FILE
+    if ! diff -q ${SAMPLES_PATH}/${X}$SAMPLE_SUFFIX_OUT ./$OUTPUT_FILE ; then
+	echo "Fail: ${X}$SAMPLE_SUFFIX_IN"
 	exit 1
     fi
 done
